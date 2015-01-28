33ac6e7e21c59767564154e19eb975810d9cf53b
Jon Williams <jonathan.williams@gmail.com>
2015-01-28 11:44:33 -0500

    follow back

--- bots.rb
+++ bots.rb
@@ -46,12 +46,12 @@ class MyBot < Ebooks::Bot
 
   def on_follow(user)
     # Follow a user back
-    # follow(user.screen_name)
+     follow(user.screen_name)
   end
 
   def on_mention(tweet)
     # Reply to a mention
-    # reply(tweet, "oh hullo")
+     #reply(tweet, "oh hullo")
   end
 
   def on_timeline(tweet)
